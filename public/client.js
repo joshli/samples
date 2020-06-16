5ab72cb92b68986c3ae6f8567d27641cd87ec225
Josh Li <joshlikang@gmail.com>
2020-06-16 03:21:48 -0400

    Add a button to capture frames and write to disk.

--- public/client.js
+++ public/client.js
@@ -3,8 +3,11 @@ var divSelectRoom = document.getElementById("selectRoom");
 var divConsultingRoom = document.getElementById("consultingRoom");
 var inputRoomNumber = document.getElementById("roomNumber");
 var btnGoRoom = document.getElementById("goRoom");
+var btnCaptureFrame = document.getElementById("captureFrame");
 var localVideo = document.getElementById("localVideo");
 var remoteVideo = document.getElementById("remoteVideo");
+
+
 
 // variables
 var roomNumber;
@@ -34,11 +37,33 @@ btnGoRoom.onclick = function () {
     }
 };
 
+btnCaptureFrame.onclick = function () {
+    setInterval(function() {
+        context = canvas.getContext('2d');
+        context.drawImage(localVideo, 0, 0, 96, 72);
+            // Get the DataUrl from the Canvas
+    const url = canvas.toDataURL('image/jpg', 0.8);
+
+    // remove Base64 stuff from the Image
+    const base64Data = url.replace(/^data:image\/png;base64,/, "");
+    window.fileSystem.writeFile('test3.png', base64Data, 'base64', function (err) {
+        if (err) throw err;
+        window.fileSystem.rename('test3.png', 'test.png', () => { 
+            console.log("\nFile Renamed!\n"); 
+        });
+     });
+    }, 1);
+};
+
 // message handlers
 socket.on('created', function (room) {
     navigator.mediaDevices.getUserMedia(streamConstraints).then(function (stream) {
         localStream = stream;
+       // const track = stream.getVideoTracks()[0];
+
         localVideo.srcObject = stream;
+
+        //let imageCapture = new ImageCapture(track);
         isCaller = true;
     }).catch(function (err) {
         console.log('An error ocurred when accessing media devices', err);
